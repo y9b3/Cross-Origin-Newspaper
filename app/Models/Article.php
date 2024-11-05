33d76b40bd2688894413fcfbf27417f305cab829
y9b3 <yanis.boudjelal@edu.ece.fr>
2024-11-05 15:49:34 +0100

    Fix : $filiable to $fillable

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Article extends Model{
     use HasFactory;
 
-    protected $filiable = [
+    protected $fillable = [
         'title',
         'author',
         'publication_date',
